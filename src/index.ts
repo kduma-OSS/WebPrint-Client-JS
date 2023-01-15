598551bba988a3bdc8c2c69db673c555f73c67d1
Krystian Duma <git@krystian.duma.sh>
2023-01-15 18:45:25 +0100

    Export as default

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {default as WebPrintClient} from './WebPrintClient';
+export {default as default} from './WebPrintClient';
 
 export {default as PrintDialogModel} from "./Models/PrintDialogModel";
 export {default as PrinterModel} from "./Models/PrinterModel";
